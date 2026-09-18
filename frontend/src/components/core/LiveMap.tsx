f391350328d64a1ee1496cf10eb8a9b0376e2140
Anshu Gaur <anshugaur718@gmail.com>
2026-09-18 09:15:57 +0530

    removed carto db from map

--- frontend/src/components/core/LiveMap.tsx
+++ frontend/src/components/core/LiveMap.tsx
@@ -606,15 +606,6 @@ export function LiveMap({
             />
           </LayersControl.BaseLayer>
 
-          <LayersControl.BaseLayer name="CartoDB Dark">
-            <TileLayer
-              url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-              attribution='&copy; OpenStreetMap &copy; <a href="https://carto.com/">CARTO</a>'
-              maxNativeZoom={20}
-              maxZoom={20}
-              subdomains="abcd"
-            />
-          </LayersControl.BaseLayer>
 
           <LayersControl.BaseLayer name="Satellite (ESRI)">
             <TileLayer
